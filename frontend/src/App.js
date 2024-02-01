245f3ac7389ac3bb13211f716d1ae92307e77ad4
vivek singh <vickydigari6154@gmail.com>
2024-02-01 17:51:02 +0530

    Created Apolllo/client instance to connect to the graphql api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,18 @@ import Sidebar from "./components/layout/Sidebar";
 import Home from "./components/pages/Home";
 import SignIn from "./auth/SignIn";
 import SignUp from "./auth/SignUp";
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client"
 
 function App() {
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri:"http://localhost:4000/graphql"
+  })
+
   return (
     <div className="App">
+      <ApolloProvider client={client}>
       <Router>
         <Routes>
           <Route
@@ -27,6 +35,7 @@ function App() {
         <Route path="/SignUp" element={ <SignUp/>}/>
         </Routes>
       </Router>
+      </ApolloProvider>
     </div>
   );
 }
